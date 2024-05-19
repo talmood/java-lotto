ffd4958d5c914972a0df8974ae611d557ba812e1
haero77 <shineecard5@gmail.com>
2024-05-19 23:05:35 +0900

    refactor: 불변 값 객체를 레코드 타입으로 변환

--- src/main/java/model/LottoTicket.java
+++ src/main/java/model/LottoTicket.java
@@ -3,13 +3,9 @@ package model;
 import java.util.List;
 import java.util.Objects;
 
-public class LottoTicket {
-
-    private final List<LottoGame> games;
-
-    public LottoTicket(List<LottoGame> games) {
-        this.games = games;
-    }
+public record LottoTicket(
+        List<LottoGame> games
+) {
 
     public int getGameSize() {
         if (Objects.isNull(this.games)) {
