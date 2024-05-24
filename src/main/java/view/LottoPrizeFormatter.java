a55e0dd7f7d21fb890a7a7d8861d354ecceff59a
haero77 <shineecard5@gmail.com>
2024-05-24 20:38:36 +0900

    refactor: optimize import

--- src/main/java/view/LottoPrizeFormatter.java
+++ src/main/java/view/LottoPrizeFormatter.java
@@ -3,7 +3,6 @@ package view;
 import model.LottoPrize;
 import model.LottoWinningResult;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
